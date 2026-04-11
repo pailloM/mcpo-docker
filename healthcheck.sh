84a77e5c3294c7acbe1c2220f737b0d2e2cc591f
PailloM <paillomams@gmx.com>
2026-04-10 23:54:56 -0400

    update

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,7 +2,7 @@
 
 response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8000)
 
-if [ "$response" -eq 200 ] || [ "$response" -eq 404 ]; then
+if [ "$response" -eq 200 ] || [ "$response" -eq 401 ]; then
   exit 0
 else
   exit 1
